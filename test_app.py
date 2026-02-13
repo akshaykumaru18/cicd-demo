9e1422a05dcaecafd847ecee2ddb3f463fa4b82a
Akshay Kumar U <akshayapsangi123@gmail.com>
2026-02-13 14:49:38 +0530

    mumbai added

--- test_app.py
+++ test_app.py
@@ -2,5 +2,5 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get('/')
-    assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+    assert response.status_code  200
+    assert response.data == b'Hello, Mumbai!'
